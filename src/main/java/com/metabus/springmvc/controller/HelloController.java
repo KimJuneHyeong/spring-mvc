b0937fbc53e041b558b95032438a61ab3a57a3aa
june <thanks7229@naver.com>
2025-07-04 15:03:10 +0900

    feat: Get/Post 요청 테스트

--- src/main/java/com/metabus/springmvc/controller/HelloController.java
+++ src/main/java/com/metabus/springmvc/controller/HelloController.java
@@ -1,12 +1,28 @@
 package com.metabus.springmvc.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
 
 public class HelloController {
+
     @GetMapping("api/hello")
     @ResponseBody
-    public String hello() {
+    public String hello(){
         return "Hello World!";
+    }
+
+    @GetMapping("api/get")
+    @ResponseBody
+    public String get(){
+        return "Get Method요청";
+    }
+
+    @PostMapping("api/post")
+    @ResponseBody
+    public String post(){
+        return "post Method 요청";
     }
 }
