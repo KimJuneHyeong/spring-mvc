5afb69f516d37e6764111d2425a49acb41c4cddc
june <thanks7229@naver.com>
2025-07-07 10:41:21 +0900

    feat: 뷰템플릿엔진 - 정적 웹 페이지 변환 redirection

---
+++ src/main/java/com/metabus/springmvc/controller/HellowViewController.java
@@ -0,0 +1,17 @@
+package com.metabus.springmvc.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HellowViewController {
+    @GetMapping("/static-hello")
+    public String hello(){
+        return "hello.html";
+    }
+    @GetMapping("/html/redirect")
+    public String htmlRedirect() {
+        return "redirect:/hello.html";
+    }
+}
