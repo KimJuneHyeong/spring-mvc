41f91679bcd8a3f494b493c76deac026d6e01293
june <thanks7229@naver.com>
2025-07-08 14:22:15 +0900

    feat:과제 내 홈페이지 만들기 기본코드 삽입

--- src/main/java/com/metabus/springmvc/controller/HelloViewController.java
+++ src/main/java/com/metabus/springmvc/controller/HelloViewController.java
@@ -4,22 +4,17 @@ package com.metabus.springmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
-public class HellowViewController {
-    @GetMapping("response/json/string")
-    @ResponseBody
-        public String helloStringJson(){
-            return "{\"name\":\"Meta\",\"age\": 95}"
-
-    }
+public class HelloViewController {
     private static long visitCount = 0;
 
     @GetMapping("/static-hello")
-    public String hello(){
+    public String hello() {
         return "hello.html";
     }
+
     @GetMapping("/html/redirect")
     public String htmlRedirect() {
         return "redirect:/hello.html";
