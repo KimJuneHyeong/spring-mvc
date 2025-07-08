ac0edf1269fad1f9ecee3b9035a9f7a98f50f442
june <thanks7229@naver.com>
2025-07-08 12:32:26 +0900

    feat: QueryString 방식 Post 요청 받기

--- src/main/java/com/metabus/springmvc/controller/DataRequestController.java
+++ src/main/java/com/metabus/springmvc/controller/DataRequestController.java
@@ -2,10 +2,7 @@ package com.metabus.springmvc.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/hello/request")
@@ -19,5 +16,20 @@ public class DataRequestController {
     @ResponseBody
     public String helloRequestPath(@PathVariable String name,@PathVariable int age){
         return String.format("Hello, @PathVariable로 받은 값들, <br> name = %s, age = %d", name, age);
+    }
+
+    // QueryString 방식 요청 받는 방법 (@RequestParam)
+    @GetMapping("/form/param")
+    @ResponseBody
+    public String helloRequestParam(@RequestParam String name,@RequestParam int age){
+        return String.format("Hello, @PathVariable로 받은 값들, <br> name = %s, age = %d", name, age);
+
+    }
+
+    @PostMapping("/format/param")
+    @ResponseBody
+    public String helloRequestParamPost(@RequestParam String name, @RequestParam int age){
+        String.format("Hello, @RequestParam으로 받은 값들 Post요청, <br> name = %s, age = %d", name, age);
+
     }
 }
